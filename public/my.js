ef2207873677f83b81e21cf07512a673faa3c36a
bellcliff <bell.cliff@gmail.com>
2016-02-01 13:40:57 -0500

    for ui control

--- public/my.js
+++ public/my.js
@@ -1,16 +1,21 @@
 (function() {
     'use strict';
-    var socket = io.connect('http://localhost:8090');
-    socket.on('news', function(data) {
-        console.log('on news', data);
-        socket.emit('my other event', {
-            my: 'data'
+    var socket; // = io.connect('http://localhost:8090');
+    var btnLeave = document.getElementById('leave');
+    var btnJoin = document.getElementById('join');
+    var divStatus = document.getElementById('status');
+    btnLeave.addEventListener('click', function() {
+        console.log('ui leave', socket)
+        socket.close();
+    });
+    btnJoin.addEventListener('click', function() {
+        console.log('ui join', socket)
+        if (!!socket && socket.connected) {
+            return;
+        }
+        socket = io.connect('http://localhost:8090')
+        socket.on('news', function() {
+            
         })
     });
-    socket.on('end', function(data){
-        console.log('on end', data)
-        setTimeout(function(){
-            socket.close();
-        }, 3000);
-    })
 })()
